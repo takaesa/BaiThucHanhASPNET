817be64ea5025e7d1a6e6f7d2db2f9ec7d4ded80
Takae <93442902+takaesa@users.noreply.github.com>
2024-11-05 15:53:45 +0700

    update edit khach hang

--- BaiThucHanhASPNET/Areas/Admin/Controllers/HomeAdminController.cs
+++ BaiThucHanhASPNET/Areas/Admin/Controllers/HomeAdminController.cs
@@ -163,14 +163,7 @@ namespace BaiThucHanhASPNET.Areas.Admin.Controllers
 
         public IActionResult SuaKhachHang(string maKhachHang)
         {
-            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
-            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSx", "HangSx");
-            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
-            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
-            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
-
-            var khachHang = db.TDanhMucSps.Find(maKhachHang);
-            return View(khachHang);
+            return View(maKhachHang);
         }
 
         [Route("SuaKhachHang")]
